d956e5a100e99e90a2ca4e704904fcdf5e2c3502
code-death <poorvanshkavta938@gmail.com>
2023-04-26 13:00:25 +0530

    State management in Customizer tab is done

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -7,8 +7,8 @@ const Backdrop = () => {
     const shadows = useRef()
   return (
     <AccumulativeShadows ref={shadows} temporal frames={60} alphaTest={0.85} scale={10} rotation={[Math.PI/2, 0, 0]} position={[0, 0, -0.14]}>
-        <RandomizedLight amount={4} radius={9} intensity={0.55} ambient={0.55} position={[5,5,-10]} />
-        <RandomizedLight amount={4} radius={5} intensity={0.25} ambient={0.55} position={[-5,5,-9]} />
+        <RandomizedLight amount={2} radius={3} intensity={.50} ambient={0.7} position={[5,5,-10]} />
+        <RandomizedLight amount={2} radius={5} intensity={0.55} ambient={0.6} position={[-5,5,-9]} />
     </AccumulativeShadows>
   )
 }
